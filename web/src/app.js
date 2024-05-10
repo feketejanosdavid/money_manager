10af36fce513c18fa1920a3efc218c1e8be0dae8
Fekete János Dávid <janosdavidfekete@gmail.com>
2024-05-10 19:24:53 +0200

    Datas are now can be appears on the webpage

---
+++ web/src/app.js
@@ -0,0 +1,42 @@
+const Doc = {
+    IncomeBody: document.getElementById('IncomeBody')
+}
+
+const State = {
+    incomesURL: "http://localhost:8000/incomes",
+    expensesURL: "http://localhost:8000/expenses"
+}
+
+function getIncomes() {
+    fetch(State.incomesURL).then(Response => Response.json()).then(result => {
+        console.log(result);
+        renderIncomes(result)
+    })
+}
+
+function getExpenses() {
+    fetch(State.expensesURL).then(Response => Response.json()).then(result => {
+        console.log(result)
+    })
+}
+
+function renderIncomes(incomeList) {
+    incomeList.forEach(data => {
+        var row = document.createElement('tr')
+        row.innerHTML = `
+        <td>${data.id}</td>
+        <td>${data.date}</td>
+        <td>${data.type}</td>
+        <td>${data.amount}</td>
+        `
+        Doc.IncomeBody.appendChild(row)
+    });
+}
+
+function getDatas() {
+    getIncomes();
+    getExpenses()
+}
+
+
+getDatas()
